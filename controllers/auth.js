89711fb5316a63b5183720d0e7f233c6c930e3ac
abram <abrambagus@gmail.com>
2025-10-02 14:03:19 +0700

    handle post new password

--- controllers/auth.js
+++ controllers/auth.js
@@ -152,7 +152,7 @@ exports.postReset = (req, res, next) => {
         return user.save();
       })
       .then(() => {
-        res.redirect("/");
+        res.redirect("/login");
         return sgMail.send({
           to: req.body.email,
           from: "abrambagus@gmail.com",
@@ -188,7 +188,35 @@ exports.getNewPassword = (req, res, next) => {
         pageTitle: "New Password",
         errorMessage: message,
         userId: user._id.toString(),
+        passwordToken: token,
       });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(() => {
+      res.redirect("/login");
     })
     .catch((err) => console.log(err));
 };
