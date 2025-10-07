ca68db3ecaecd04823ea3d9d7fc8da22094dd46e
abram <abrambagus@gmail.com>
2025-10-07 14:34:24 +0700

    refactor signup route to remove confirmPassword handling and improve email uniqueness validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -74,7 +74,7 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).render("auth/signup", {
@@ -83,39 +83,27 @@ exports.postSignup = (req, res, next) => {
       errorMessage: errors.array()[0].msg,
     });
   }
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash(
-          "error",
-          "E-Mail exists already, please pick a different one."
-        );
-        return res.redirect("/signup");
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: {
-              items: [],
-            },
-          });
-          return user.save();
-        })
-        .then(() => {
-          res.redirect("/login");
-          return sgMail.send({
-            to: email,
-            from: "abrambagus@gmail.com",
-            subject: "Signup succeeded",
-            html: "<h1>You successfully signed up!</h1>",
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: {
+          items: [],
+        },
+      });
+      return user.save();
+    })
+    .then(() => {
+      res.redirect("/login");
+      return sgMail.send({
+        to: email,
+        from: "abrambagus@gmail.com",
+        subject: "Signup succeeded",
+        html: "<h1>You successfully signed up!</h1>",
+      });
     })
     .catch((err) => {
       console.log(err);
